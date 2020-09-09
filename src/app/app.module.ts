09ae05b31e29c5bc5eca832b622d33c79d31767a
AKSHYA1997 <akshyaravishankar@gmail.com>
2020-09-09 21:17:36 +0530

    :sparkles: added navbar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule,
