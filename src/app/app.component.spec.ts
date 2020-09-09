a58b4b2b8ec9484caa3cb4f691b9043561eb0bf1
AKSHYA1997 <akshyaravishankar@gmail.com>
2020-09-09 20:56:51 +0530

    :sparkles: adding bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('thoven-ui');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('thoven-ui app is running!');
-  });
 });
